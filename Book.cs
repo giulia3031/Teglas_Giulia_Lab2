a24d870384f3c30acbd5cd457e1c6de870c77cb8
giulia3031 <115947607+giulia3031@users.noreply.github.com>
2022-11-13 20:11:28 +0200

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Teglas_Giulia_Lab2.Models
+{
+    public class Book
+        { 
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+        }
+    }
